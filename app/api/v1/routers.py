7a16ca4b9aa6570d22c5a2fc8f7b0581eb3741dd
seokyung <lastsun4@naver.com>
2025-09-15 12:33:20 +0900

    main : 덕재 source 추가-2

--- app/api/v1/routers.py
+++ app/api/v1/routers.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter
 from app.api.v1.endpoints import item
+from app.api.v1.endpoints import ImgUpload
 
 # API 라우터 설정
 api_router = APIRouter()
 
 # 아이템 관련 API 엔드포인트 포함
 api_router.include_router(item.router, prefix="/items", tags=["items"])
+
+# 라즈베리파이로부터 이미지 받는 엔드포인트
+api_router.include_router(ImgUpload.router, prefix="/uploadImg", tags=["Imgs"])
 
 # API 엔드포인트 추가
