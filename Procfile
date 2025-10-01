1812036be4976c50395add24b9b53a6f57d9c89e
seokyung <lastsun4@naver.com>
2025-10-01 19:37:16 +0900

    remove git cache

--- Procfile
+++
@@ -1,5 +0,0 @@
-web: gunicorn -k uvicorn.workers.UvicornWorker app.main:app \
-  --bind 0.0.0.0:8000 \
-  --workers 2 \
-  --timeout 120 \
-  --keep-alive 30
--- app/models/parkingLot.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, ConfigDict
-
-class parkingSpotOut(BaseModel):
-    lot_code : str
-    spot_row : int
-    spot_column : int
-    occupied_cd : str
-    model_config = ConfigDict(from_attributes=True)
